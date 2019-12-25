17f41e501c6a173e51e7cd3652a04de3512810db
niepu <niepu1991@163.com>
2019-12-25 23:02:44 +0800

    DeferredImportSelector实现Ordered接口

--- source-code-spring/src/main/java/com/example/spring/auto/MyDeferredImportSelector.java
+++ source-code-spring/src/main/java/com/example/spring/auto/MyDeferredImportSelector.java
@@ -1,12 +1,13 @@
 package com.example.spring.auto;
 
 import org.springframework.context.annotation.DeferredImportSelector;
+import org.springframework.core.Ordered;
 import org.springframework.core.type.AnnotationMetadata;
 
 /**
  * @date:2019/12/23 22:23
  **/
-public class MyDeferredImportSelector implements DeferredImportSelector {
+public class MyDeferredImportSelector implements DeferredImportSelector, Ordered {
 
     public MyDeferredImportSelector() {
         System.out.println("MyDeferredImportSelector构造器------------------");
@@ -18,4 +19,9 @@ public class MyDeferredImportSelector implements DeferredImportSelector {
         return new String[]{MyDeferredImport1.class.getName()};
     }
 
+    // 加上与AutoConfigurationImportSelector一样的优先级
+    @Override
+    public int getOrder() {
+        return Ordered.LOWEST_PRECEDENCE - 1;
+    }
 }
--- source-code-spring/src/main/java/com/example/spring/auto/import2/MyDeferredImportSelector2.java
+++ source-code-spring/src/main/java/com/example/spring/auto/import2/MyDeferredImportSelector2.java
@@ -1,9 +1,10 @@
 package com.example.spring.auto.import2;
 
 import org.springframework.context.annotation.DeferredImportSelector;
+import org.springframework.core.Ordered;
 import org.springframework.core.type.AnnotationMetadata;
 
-public class MyDeferredImportSelector2 implements DeferredImportSelector {
+public class MyDeferredImportSelector2 implements DeferredImportSelector, Ordered {
 
     public MyDeferredImportSelector2() {
         System.out.println("MyDeferredImportSelector2构造器------------------");
@@ -15,4 +16,9 @@ public class MyDeferredImportSelector2 implements DeferredImportSelector {
         return new String[]{MyDeferredImport2.class.getName()};
     }
 
+    // 加上与AutoConfigurationImportSelector一样的优先级
+    @Override
+    public int getOrder() {
+        return Ordered.LOWEST_PRECEDENCE - 1;
+    }
 }
